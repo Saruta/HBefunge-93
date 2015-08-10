bebdd44c3899c90e1edddb27f88e9378c73402b3
Saruta <fsiron@orange.fr>
2015-08-10 12:49:46 +0000

    hello world works

--- Interpreter.hs
+++ Interpreter.hs
@@ -6,6 +6,7 @@ module Interpreter where
 import qualified Parser
 import Stack
 import Data.Vector
+import Data.Char
 import qualified Data.List as Lst
 
 controlFlow :: Parser.Grid -> (Stack Int)-> (Int,Int) -> Int -> Int 
@@ -28,16 +29,16 @@ arithm (Parser.Grid grid w h)  s  (dx,dy) line column =
       let s''' = push (x+y) s'' in
         step (Parser.Grid grid w h) s''' (dx,dy) (line+dy) (column+dx)
     '-' -> let (x,s') = pop s in let (y,s'') = pop s' in
-      let s''' = push (x+y) s'' in
+      let s''' = push (y-x) s'' in
         step (Parser.Grid grid w h) s''' (dx,dy) (line+dy) (column+dx)
     '*' -> let (x,s') = pop s in let (y,s'') = pop s' in
-      let s''' = push (x+y) s'' in
+      let s''' = push (x*y) s'' in
         step (Parser.Grid grid w h) s''' (dx,dy) (line+dy) (column+dx)
     '/' -> let (x,s') = pop s in let (y,s'') = pop s' in
-      let s''' = push (x+y) s'' in
+      let s''' = push (y `quot` x) s'' in
         step (Parser.Grid grid w h) s''' (dx,dy) (line+dy) (column+dx)
     '%' -> let (x,s') = pop s in let (y,s'') = pop s' in
-      let s''' = push (x+y) s'' in
+      let s''' = push (y `mod` x) s'' in
         step (Parser.Grid grid w h) s''' (dx,dy) (line+dy) (column+dx)
 --
 --
@@ -45,9 +46,18 @@ user :: Parser.Grid -> (Stack Int) -> (Int,Int) -> Int -> Int -> IO()
 user (Parser.Grid grid w h) s (dx,dy) line column = 
   case ((grid ! line)! column) of
     '.' -> let (x,s') = pop s in
-      do {print x;
+      do {print $ x;
       step (Parser.Grid grid w h) s' (dx,dy) (line+dy) (column+dx)}
--- misc
+    ',' -> let (x,s') = pop s in
+      do {putChar $ chr x;
+      step (Parser.Grid grid w h) s' (dx,dy) (line+dy) (column+dx)}
+
+string :: Parser.Grid -> (Stack Int) -> (Int,Int) -> Int -> Int ->IO()
+string (Parser.Grid grid w h) s (dx,dy) line column =
+  case ((grid ! line)! column) of
+    '"' -> step (Parser.Grid grid w h) s (dx,dy) (line+dy) (column+dx)
+    c -> string (Parser.Grid grid w h) (pushChar c s) (dx,dy)   
+      (line+dy) (column+dx)
 
 --        grid                    sens         line  column
 step :: Parser.Grid -> (Stack Int) -> (Int,Int) -> Int -> Int -> IO()
@@ -58,10 +68,12 @@ step (Parser.Grid grid w h) s (dx,dy) line column =
     c | Lst.elem c ['0'..'9']
       -> step (Parser.Grid grid w h) (pushInt c s) (dx,dy)
       (line+dy) (column+dx)
-    c | Lst.elem c ['+']
+    c | Lst.elem c ['+','-','*','/','%']
       -> arithm (Parser.Grid grid w h) s (dx,dy) line column
-    c | Lst.elem c ['.']
+    c | Lst.elem c ['.',',']
       -> user (Parser.Grid grid w h) s (dx,dy) line column
+    '"' -> string (Parser.Grid grid w h) s (dx,dy) (line+dy) 
+        (column+dx)
     _ -> putStrLn "error, instruction not implemented"
 
 run :: Parser.Grid -> IO()
