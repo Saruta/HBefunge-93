ca26e724206b9d0e3d2346a39e012bda22c0f58b
Saruta <fsiron@orange.fr>
2015-08-11 13:38:27 +0000

    resolve bug grid

--- Parser.hs
+++ Parser.hs
@@ -12,15 +12,21 @@ data Grid = Grid
   Int -- Width
   Int -- Height
 
+expand :: [Char] -> [Char]
+expand line_ = line_ ++ (replicate (80 - (length line_)) ' ') 
+
 -- grid :
 --      * width  <-> length $ grid !! 0
 --      * height <-> length grid
 parse :: [Char] -> Grid
 parse txt = 
-  let grid = lines txt 
-    in Grid 
-      (Data.Vector.generate (length grid) 
-        (\n -> Data.Vector.fromList $ grid !! n)) 
-      (length $ grid !! 0)
-      (length grid)
+  let grid = map expand $lines txt in
+    let len = length grid in
+      Grid 
+        (Data.Vector.generate 25 
+          (\n ->  if n < len then 
+                    Data.Vector.fromList $ grid !! n
+                  else 
+                    Data.Vector.replicate 80 ' ')) 
+            80 25
 
